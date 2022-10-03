ddb17f6cbab14522921ced7779a00c96e913abdb
han <pure33883833@gmail.com>
2022-10-04 02:48:03 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+// 앱 실행 하자마자 현재 위치 기반의 날씨가 보임
+// 현재 도시, 섭, 화씨, 날씨상태 보인다
+// 5개 버튼이 보인다
+// 버튼 클릭하면 그 도시의 날씨 보인다
+// 현재위치 버튼누르면 다시 현재위치 날씨
+// 로딩 스피너
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
     </div>
   );
 }
