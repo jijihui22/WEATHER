3b3ccbb4ac2b360b714384a9757bc1c9c34ac778
han <pure33883833@gmail.com>
2022-10-06 03:05:45 +0900

    00

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 // 앱 실행 하자마자 현재 위치 기반의 날씨가 보임
 // 현재 도시, 섭, 화씨, 날씨상태 보인다
@@ -7,9 +7,15 @@ import './App.css';
 // 현재위치 버튼누르면 다시 현재위치 날씨
 // 로딩 스피너
 function App() {
+  const getCurrentLocation = () => {
+    console.log("getCurrentLocation")
+  }  
+  useEffect(() => {
+      getCurrentLocation()
+  },[])
   return (
     <div>
-      
+      ㄷㄷ
     </div>
   );
 }
