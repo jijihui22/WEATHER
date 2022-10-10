f4ec6425f082270ea0d4f02fac235783b0599eb4
han <pure33883833@gmail.com>
2022-10-11 04:54:45 +0900

    complete

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Button } from 'react-bootstrap';
 
 const WeatherButton = ({cities, setCity}) => {
   return (
     <div>
-        <Button variant="warning">Current Location</Button>
+        <Button variant="warning" onClick={() => {window.location.href='/'}}>Current Location</Button>
         {cities.map((item, index) => (
-          <Button variant="warning" key={index} onClick={() => setCity(item)}>{item}</Button>
+          <Button variant="warning" onClick={() => setCity(item)}>{item}</Button>
         ))}
     </div>
   )
-}
+};
 
 export default WeatherButton
